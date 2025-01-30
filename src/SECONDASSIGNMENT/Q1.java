64d52aa652c361a6735050c9b3faec8850990e3d
AMIT <amitkumargiri2005@gmail.com>
2025-01-31 01:37:55 +0530

    FSEM

---
+++ src/SECONDASSIGNMENT/Q1.java
@@ -0,0 +1,11 @@
+package SECONDASSIGNMENT;
+import java.util.*;
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double celcius;
+        double f  = sc.nextDouble();
+        celcius= (f -32)*(5.0/9);
+        System.out.println(celcius);
+    }
+}
