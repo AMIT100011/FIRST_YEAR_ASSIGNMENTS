ce279da910b46d6fd88235a17921ef046bcf099a
AMIT <amitkumargiri2005@gmail.com>
2025-01-31 01:48:01 +0530

    FSEM

---
+++ src/SEVENTHASSIGNMENT/Q1.java
@@ -0,0 +1,36 @@
+package SEVENTHASSIGNMENT;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number = ");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        average(arr);
+
+
+    }
+
+    private static void average(int[] arr) {
+        int totsum = sum(arr);int n = arr.length;
+        System.out.println(totsum);
+        double avg;
+        avg = totsum / n;
+        System.out.println(avg);
+
+
+    }
+
+    private static int sum(int[] arr) {
+int sum = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+}
---
+++ src/SEVENTHASSIGNMENT/Q6.java
@@ -0,0 +1,34 @@
+package SEVENTHASSIGNMENT;
+
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number = ");
+        int[] arr = new int[8];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        reverse(arr);
+    }
+
+    private static void reverse(int[] arr) {
+        int temp = 0;int i = 0;int j = arr.length - 1; ;
+        while( i < j){
+               swap(i,j,arr);
+                       i++;j--;
+        }
+        for (int k : arr) {
+            System.out.print(k + " ");
+        }
+        System.out.println();
+    }
+
+    private static void swap(int  i, int j,int [] arr) {
+        int temp =0;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
