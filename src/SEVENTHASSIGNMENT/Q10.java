c9e7844f4658e707116499219d9a7325fc8347c7
AMIT <amitkumargiri2005@gmail.com>
2025-01-31 14:27:24 +0530

    FSEM

--- src/SEVENTHASSIGNMENT/Q10.java
+++ src/SEVENTHASSIGNMENT/Q10.java
@@ -1,38 +1,43 @@
 package SEVENTHASSIGNMENT;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Q10 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the array");
-        int[] arr = new int[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int newArr = duplicates(arr);
-        for (int i = 0; i < newArr; i++) {
-             System.out.print(arr[i] + " ");
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[10];
+
+        System.out.print("Enter ten numbers: ");
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = input.nextInt();
         }
 
+        int[] distinctNumbers = eliminateDuplicates(numbers);
+
+        System.out.print("The distinct numbers are: ");
+        for (int num : distinctNumbers) {
+            System.out.print(num + " ");
+        }
     }
 
-    private static int duplicates(int[] arr) {
-        int [] temp = new int[arr.length];
-        int j = 0;
-        for (int i = 0; i < arr.length-1 ; i++) {
-            for (int k = 0; k < arr.length  ; k++) {
-                if(arr[i] != arr[k]){
-                    temp[j++] = arr[i];
+    public static int[] eliminateDuplicates(int[] list) {
+        int[] temp = new int[list.length];
+        int count = 0;
+
+        for (int i = 0; i < list.length; i++) {
+            boolean isDuplicate = false;
+            for (int j = 0; j < count; j++) {
+                if (list[i] == temp[j]) {
+                    isDuplicate = true;
                     break;
                 }
             }
+            if (!isDuplicate) {
+                temp[count++] = list[i];
+            }
         }
-        temp[j++] = arr[arr.length-1];
-        for (int i = 0; i < j ; i++) {
-            arr[i] = temp[i];
-        }
-        return j;
-    }
 
+        return Arrays.copyOf(temp, count);
+    }
 }
