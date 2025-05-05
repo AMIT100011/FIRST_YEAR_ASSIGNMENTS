119b917c719d46c45013a92a34a0bc9122750bf3
AMIT100011 <amitkumargiri2005@gmail.com>
2025-05-05 11:24:11 +0530

    Update Q11.java

--- FIRST_YEAR/SECOND_SEM/THIRD_ASSIGNMENT/Q11.java
+++ FIRST_YEAR/SECOND_SEM/THIRD_ASSIGNMENT/Q11.java
@@ -2,7 +2,7 @@ public class Q11 {
   static int gcd(int x, int y) {
     if(y==0)
       return x;
-    return gcd(x, x % y);
+    return gcd(y, x % y);
   }
   public static void main(String[] args) {
 System.out.println("GCD = " + gcd(48, 18));
